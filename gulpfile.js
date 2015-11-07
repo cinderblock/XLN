b4dee4864ba68b6262fd711299c7b3c879d65118
Cameron Tacklind <cameron@tacklind.com>
2015-11-07 01:55:01 -0800

    Adding gulp and the ability to export to a dist directory and publish to npm an ES5'd package

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const spawn = require('child_process').spawn;
+const jeditor = require('gulp-json-editor');
+
+const npmDistDir = 'dist';
+
+gulp.task('babel', () => {
+  return gulp.src('XLN.js')
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(gulp.dest(npmDistDir));
+});
+
+gulp.task('package.json', () => {
+  return gulp.src('package.json')
+    .pipe(jeditor(json => {
+      // Strip devDependencies from published .json
+      json.devDependencies = undefined;
+      return json;
+    }))
+    .pipe(gulp.dest(npmDistDir));
+});
+
+gulp.task('dist', ['package.json', 'babel']);
+
+gulp.task('publish', ['dist'], done => {
+  spawn('npm', ['publish'], {cwd: npmDistDir, stdio: 'inherit'}).on('close', done);
+});
+
+gulp.task('default', ['dist']);
