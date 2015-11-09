0843cbb001d6d258b20740eb01baf36cded3d0b7
Cameron Tacklind <cameron@tacklind.com>
2015-11-09 00:26:45 -0800

    Publishing both versions (es5 & es2015) of the source

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
-const jeditor = require('gulp-json-editor');
 
 const npmDistDir = 'dist';
 
@@ -12,16 +11,6 @@ gulp.task('babel', () => {
     .pipe(gulp.dest(npmDistDir));
 });
 
-gulp.task('package.json', () => {
-  return gulp.src('package.json')
-    .pipe(jeditor(json => {
-      // Strip devDependencies from published .json
-      json.devDependencies = undefined;
-      return json;
-    }))
-    .pipe(gulp.dest(npmDistDir));
-});
-
-gulp.task('dist', ['package.json', 'babel']);
+gulp.task('dist', ['babel']);
 
 gulp.task('default', ['dist']);
